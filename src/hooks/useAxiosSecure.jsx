867b011fb410ebcaeb118397e2a0b1d8b0a61bf2
maishaj <maishajannat388@gmail.com>
2026-03-01 00:12:32 +0600

    fixed dark mode

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -4,8 +4,7 @@ import useAuth from "./useAuth";
 import { useNavigate } from "react-router";
 
 const axiosSecure = axios.create({
-  // baseURL: "https://local-chef-bazaar-server-nine.vercel.app",
-  baseURL: "http://localhost:3000",
+  baseURL: "https://local-chef-bazaar-server-nine.vercel.app",
 });
 
 const useAxiosSecure = () => {
